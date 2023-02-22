eb643fda6a20636ed4cc612ee5fae72f2dbf6a9c
thombator <thom.bator@gmail.com>
2023-02-22 18:36:54 -0500

    removed supabase-config file as it is no longer needed in the new decoupled front-end version of the site

--- src/config/supabase.client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import "vite/client";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_API_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
